3556f2c2fd013ef3b7a43290145fce7836d7a693
Uros Zigic <ukizigic@gmail.com>
2025-02-27 16:11:09 +0100

    Initialization project

---
+++ app.py
@@ -0,0 +1,50 @@
+import random
+import sys
+
+def subnet_calc(ip_addres):
+    try:
+        print("\n")
+        # Convert an address into octets ->|
+        ip_octets = ip_address.split('.')
+
+        if len(ip_octets) != 4:
+            return False
+        
+        # Convert octets into integers (casting)
+        octets = [int(octets) for octet in ip_octets]
+
+        # Ensure all octets are within 0-255
+        if not all(0 <= octet <= 255 for octet in octets):
+            return False
+        
+        first = octets[0]
+        second =  octets[1]
+
+        if  (first == 10) or \
+            (first == 172 and 16 <= second <= 31) or \
+            (first == 192 and 168):
+                return False
+        
+        if first == 127:
+            return "Loopback address"
+        
+        if first == 169 and second == 254:
+            return "APIPA (DHCP Auto-Assign)"
+        
+        if 224 <= 239:
+            return "Multicast Address IP"
+        
+        if 240 < 255:
+            return "Experimental Address/Reserved"
+        
+
+        if 1 <= first <= 223:
+            return "Public IP"
+
+        return False # Anything else is invalid
+
+    except ValueError:
+        return False
+
+while True:
+    ip_address =  input("## Please Enter your Desired ip address ##")
